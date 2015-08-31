1452d6a3deb7596d1b218c0929ab2422260b0582
Joe Howse <josephhowse@nummist.com>
2015-08-31 12:38:15 -0300

    Clarifying forward compatibility comment

--- chapter_2/CameraTrap/CVBackwardCompat.py
+++ chapter_2/CameraTrap/CVBackwardCompat.py
@@ -3,7 +3,8 @@ import cv2
 
 CV_MAJOR_VERSION = int(cv2.__version__.split('.')[0])
 if CV_MAJOR_VERSION < 3:
-    # Create aliases to make OpenCV 2.x code forward-compatible.
+    # Create aliases to make parts of the OpenCV 2.x library
+    # forward-compatible.
     cv2.createBackgroundSubtractorMOG2 = cv2.BackgroundSubtractorMOG2
     cv2.CAP_PROP_FRAME_WIDTH = cv2.cv.CV_CAP_PROP_FRAME_WIDTH
     cv2.CAP_PROP_FRAME_HEIGHT = cv2.cv.CV_CAP_PROP_FRAME_HEIGHT
