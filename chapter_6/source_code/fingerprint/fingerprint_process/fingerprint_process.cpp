61c52c29a1f1502e1289a8a7799eaaec4c9ecad6
Steven Puttemans <steven.puttemans@kuleuven.be>
2018-10-17 11:58:50 +0200

    Update fingerprint_process.cpp
    Add a check on an existing input image

--- chapter_6/source_code/fingerprint/fingerprint_process/fingerprint_process.cpp
+++ chapter_6/source_code/fingerprint/fingerprint_process/fingerprint_process.cpp
@@ -74,6 +74,9 @@ int main( int argc, const char** argv )
     // Read in an input image - directly in grayscale CV_8UC1
     // This will be our test fingerprint
     Mat input = imread("/data/fingerprints/101_1.tif", IMREAD_GRAYSCALE);
+    if(input.empty()){
+	    cerr << "Image not read correctly. Check if path is correct!" << endl;
+    }
 
     // Binarize the image, through local thresholding
     Mat input_binary;
